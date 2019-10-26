e6584ea69a97df2a61b453ad59f064d30a1f4325
Oleg <oleon95@gmail.com>
2019-10-26 15:19:01 +0300

    add reservation page with reservation form

---
+++ src/components/Datepicker.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-date-picker title-position="left"
+    :min-date='date' v-model="date" is-inline @click="getValue ()"
+  />
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      date: new Date(),
+      value: 'oo'
+    };
+  },
+  methods: {
+      
+        getValue () {
+            this.console.log(this.date)
+        }
+  
+    }
+};
+</script>
+
+<style>
+
+</style>
