d653e86299fb3072cf94f6f4715c945992ffafc1
andpaj <andpaj@ttu.ee>
2019-10-26 08:27:58 +0300

    Edited Front

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,7 +1,24 @@
 <template>
   <div class="about">
-    <h1>{{ msg }}</h1>
+    <div class="heading"><h1>{{ msg }}</h1></div>
+    <b-container class="text">
+      <b-row>
+        <b-col>Our barbershop has appeared due to the project of our university and inspired by Tallinn barbershop "Truman"
+          <b-img src="https://www.ttu.ee/public/u/ulikool/Tunnusgraafika/Logod/TTU_lyhend/Peamine/TTU_logo_lyhend_peamine_EST_print.jpg" fluid alt="Responsive image" class="picture"></b-img>
+        </b-col>
+        <b-col>We have chosen this topic because nowadays it is very popular industry. We offer you a web application that will gift you the best haircut in your life
+          <b-img src="https://s3-media0.fl.yelpcdn.com/bphoto/d5jrXTgjiCKDTLS5sqWJgw/ls.jpg" fluid alt="Responsive image"></b-img>
+        </b-col>
+
+        <b-col>
+          Front-end created using Vue js and Bootstrap
+          <b-img src="https://www.fullstackpython.com/img/logos/vuejs-wide.png" fluid alt="Responsive image"></b-img>
+        </b-col>
+
+      </b-row>
+    </b-container>
   </div>
+
 </template>
 
 <script>
@@ -9,12 +26,20 @@ export default {
     name: 'about',
     data () {
         return {
-        msg: 'Here you put about barbershop information'
+        msg: 'About us '
         }
     }
 }
 </script>
 
 <style>
-
+.text{
+  font-size: 20px;
+}
+  .picture{
+  padding-bottom: 100px;
+}
+  .heading{
+    padding-bottom: 80px;
+  }
 </style>
--- src/components/Services.vue
+++ src/components/Services.vue
@@ -1,20 +1,79 @@
 <template>
   <div class="services">
-    <h1>{{ msg }}</h1>
+    <div class="greeting">
+      <h1>List of services</h1>
+    </div>
+    <div class="services_list" v-if="Object.keys(main_services).length !== 0">
+      <div id="list__of__serveces">
+        <div role="tablist" >
+          <b-card  id="list-accordion" no-body class="mb-1">
+
+            <div v-for="(service, index) in main_services" :key="index">
+              <b-card-header header-tag="header" class="p-1" role="tab">
+                <b-btn block href="#" v-b-toggle="'accordion-' +index" variant="light" class="text-left">
+                <span class="when-opened float-right">
+                  <i class="fas fa-caret-down"></i>
+                </span>
+                  <span class="when-closed float-right">
+                    <i class="fas fa-caret-left"></i>
+                </span>
+
+                  <em class="service-name text--sans">{{service}} </em>
+                </b-btn>
+              </b-card-header>
+
+            </div>
+          </b-card>
+        </div>
+      </div>
+
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'services',
-    data () {
-        return {
-        msg: 'Here you put service list'
-        }
+  name: 'services_list',
+  data() {
+    return {
+
+      main_services: ["JUUKSELÕIKUS", "HABEMEAJAMINE", "HABEME JA VUNTSIDE KUJUNDAMINE"],
     }
+  }
 }
+
+    /**,
+  methods: {
+
+    //Тут идет запрос в базу и получение данных и запись в массив
+    getPersons: function () {
+      axios.get('/api/curators')
+              .then((response) => {
+              })
+              .catch((error) => {
+
+              })
+    }
+  },
+
+  created () {
+
+    this.services = this.$store.getters.getCurators
+    this.$store.dispatch('fetchCurators')
+            .then((data) => {
+              this.services = data
+            })
+            .catch(err => {
+
+            })
+  }
+
+}
+ */
 </script>
 
 <style>
+
+
 
 </style>
