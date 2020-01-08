19b8f5c39fbf5ddd043008aace042886c473b491
melkri <melkri@ttu.ee>
2020-01-08 13:56:54 +0200

    Added design feature into services

--- src/components/Services.vue
+++ src/components/Services.vue
@@ -6,7 +6,9 @@
     <div class="services_list" v-if="Object.keys(services).length !== 0">
       <div id="list__of__serveces">
         <b-list-group class="text-left">
-          <b-list-group-item v-for="(service, index) in services" :key="index">{{ service.serviceName }}</b-list-group-item>
+          <b-list-group-item v-for="(service, index) in services" :key="index">{{ service.serviceName }}
+            <b-badge class="float-right"  pill>~{{ service.duration }} min.</b-badge>
+          </b-list-group-item>
         </b-list-group>
       </div>
     </div>
