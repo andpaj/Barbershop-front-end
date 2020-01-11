ef0d4ff5e8e45d8df2923f471064ab3a658a20d8
Oleg <oleon95@gmail.com>
2020-01-11 15:13:00 +0200

    change style for pricelist component, delete Services component, change column order on reservation page

--- src/components/Pricelist.vue
+++ src/components/Pricelist.vue
@@ -1,32 +1,38 @@
 
 <template>
 <div class="services">
-  <div class="greeting">
-    <h1>Pricelist</h1>
-  </div>
-  <div class="services_list" v-if="Object.keys(services).length !== 0">
-    <div id="list__of__serveces">
-      <div role="tablist" >
-        <b-card  id="list-accordion" no-body class="mb-1">
-
-          <div v-for="(service, index) in services" :key="index">
-            <b-card-header header-tag="header" class="p-1" role="tab">
-              <b-btn block href="#" v-b-toggle="'accordion-' +index" variant="light" class="text-left">
-                <span class="when-opened float-right">
-                  <i class="fas fa-caret-down"></i>
-                </span>
-                <span class="when-closed float-right">
-                    <i class="fas fa-caret-left"></i>
-                </span>
-                <em class="service-name text--sans">{{ service.serviceName }} - {{ service.price }} euro</em>
-              </b-btn>
-            </b-card-header>
+  <div class="row justify-content-center">
+      <div class="col-12">
+        <div class="bg-img-container">
+          <div class="bg-img">
+            <div class="bg-header-container">
+              <h1>Services</h1>
+            </div>
           </div>
-        </b-card>
+        </div>
       </div>
     </div>
-
-  </div>
+    <b-row class="row justify-content-center">
+      <b-col cols="12" md="8" class="content-container">
+        <div class="header-container">
+          <h2> Our Services</h2>
+        </div>
+        <b-alert show variant="info" class="alert-container">We offer -20% discount for pensioners,students and childs</b-alert>
+        <b-card class="edit-tabs-container">
+          <b-card-title>Pricelist</b-card-title>
+          <b-card-sub-title class="mb-2">Juukselõikus sis. pesu</b-card-sub-title>
+          <b-list-group flush class="services-list">
+            <b-list-group-item v-for="(service, index) in services" :key="index">
+              <div class="d-flex w-100 justify-content-between">
+                <h5 class="mb-1" :value="service.serviceName">{{ service.serviceName }}</h5>
+                <h5 class="price-font">{{ service.price }} EUR.</h5>
+              </div>
+            </b-list-group-item>
+            <hr>
+          </b-list-group>
+        </b-card>
+      </b-col>
+    </b-row>
 </div>
 </template>
 
@@ -51,8 +57,31 @@
   }
 </script>
 
-<style>
+<style scoped>
 
+.alert-container {
+  margin-top: 10px; 
+}
 
+.services-list {
+  margin-top: 30px;
+}
+
+.price-font {
+  margin-left: 15px;
+  white-space: nowrap;
+}
+
+@media only screen and (max-width: 700px) {
+
+  .alert-container {
+    margin-top: 0; 
+  }
+
+  .content-container {
+    margin: 0 auto;
+  }
+
+}  
 
 </style>
